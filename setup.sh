57b37484c63e72b8497181b3e8367839975d3832
RetroZelda <efolckemer@retrozelda.com>
2024-07-13 18:32:27 -0400

    dont use symlinks when setting up systemd

--- setup.sh
+++ setup.sh
@@ -10,12 +10,10 @@ wifi_script="$current_directory/run_with_wifi.sh"
 # timers
 cron="0 * * * * "
 timer_rule="OnBootSec=1s
-OnActiveSec=1h"
+OnUnitActiveSec=1h"
 
-unit_file="$current_directory/photo-display.service"
-timer_file="$current_directory/photo-display.timer"
-unit_link="$systemd_directory/photo-display.service"
-timer_link="$systemd_directory/photo-display.timer"
+unit_file="$systemd_directory/photo-display.service"
+timer_file="$systemd_directory/photo-display.timer"
 
 install_application() {
     echo "Installing environment..."
@@ -78,15 +76,8 @@ WantedBy=timers.target
 
     # Write the unit content to the unit file
     echo "Creating systemd service"
-    echo "$unit_content" | tee "$unit_file" > /dev/null
-    echo "$timer_content" | tee "$timer_file" > /dev/null
-
-    # make the link
-    echo "Linking $unit_file to $unit_link"
-    sudo ln -s $unit_file $unit_link
-
-    echo "Linking $timer_file to $timer_link"
-    sudo ln -s $timer_file $timer_link
+    echo "$unit_content" | sudo tee "$unit_file" > /dev/null
+    echo "$timer_content" | sudo tee "$timer_file" > /dev/null
 
     echo "Reloading systemd daemon"
     sudo systemctl daemon-reload
@@ -95,16 +86,17 @@ WantedBy=timers.target
     sudo systemctl enable --now photo-display.timer
 
     echo "Running Once"
+    sudo systemctl restart photo-display.timer
     sudo systemctl start photo-display.service
 }
 
 remove_systemd_service() {
 
     echo "Removing systemd services and timers"
-    sudo rm $timer_link
-    sudo rm $unit_link
-    sudo rm $timer_file
-    sudo rm $unit_file
+    sudo systemctl stop photo-display.timer
+    sudo systemctl disable photo-display.timer
+    sudo rm -f $timer_file
+    sudo rm -f $unit_file
 
     echo "Reloading systemd daemon"
     systemctl daemon-reload
